5894140b270c96e38bf118da735f23242786122d
catalin95 <catalinmucica@gmail.com>
2019-05-19 19:46:53 +0300

    Update and rename work49.py to work5.py

--- work5.py
+++ work5.py
@@ -1,38 +1,28 @@
-class Queve:
+class Queve:	# class for creating queve objects
 
 	def __init__(self):
 		self.queve = []
 
-	def add(self, item):
+	def add(self, item):	# function to add a new item to the stak
 		return self.queve.append(item)
 
-	def remove(self):
+	def remove(self):	# function to remove an item from stack
 		if not self.queve:
 			print ('The queve is empty')
 		else:
 			return self.queve.remove(self.queve[0])
 
-	def print_first_item(self):
+	def print_first_item(self):	# function to print the first item from a stack
 		if not self.queve:
 			print ('The queve is empty')
 		else:
 			print (self.queve[0])
 			return True
 
-	def print_queve(self):
+	def print_queve(self):		# function to print the queve
 		if not self.queve:
 			print ('The queve is empty')
 		else:
 			print (self.queve)
 			return True
 
-Aqueve = Queve()
-
-Aqueve.add(1)
-Aqueve.add(10)
-Aqueve.add(20)
-Aqueve.print_queve()
-Aqueve.print_first_item()
-Aqueve.remove()
-Aqueve.print_first_item()
-Aqueve.print_queve()
