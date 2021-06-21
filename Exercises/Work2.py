f847bd983a68270df575c486c2f4ce44b7709bd1
mint <catalinmucica.test@gmail.com>
2021-06-21 15:59:40 +0300

    Adding class and changing implementation accordingly

--- Exercises/Work2.py
+++ Exercises/Work2.py
@@ -1,13 +1,31 @@
 #! Python3
 # Function for conbining 2 lists
-	
 
-	def combine(x, y):
-		if not x and y:
-			return y
-		if not y and x:
-			return x
-		if not x and not y:
-			return 'Both lists are empty'
+class Operations(object):
+
+	def __init__(self, x):
+		self.x = x
+
+
+	def combine(self, other):
+		if not self.x and not other.x:
+			return other.x
+		elif not other.x and self.x:
+			return self.x
+		elif not self.x and not other.x:
+			return "Both lists are empty"
 		else:
-			return x + y
+			new_object = Operations(self.x + other.x)
+
+
+'''
+def combine(x, y):
+	if not x and y:
+		return y
+	if not y and x:
+		return x
+	if not x and not y:
+		return 'Both lists are empty'
+	else:
+		return x + y
+'''
