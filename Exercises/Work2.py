46af599fdda1d6d57f6c28459129258ff125fa04
mint <catalinmucica.test@gmail.com>
2021-06-21 15:42:23 +0300

    Modifying file

--- Exercises/Work2.py
+++ Exercises/Work2.py
@@ -1,12 +1,17 @@
 #! Python3
 # Function for conbining 2 lists
 
-def combine(x, y): 
-	if not x and y:
-		return y
-	if not y and x:
-		return x
-	if not x and not y:
-		return 'Both lists are empty'
-	else:
-		return x + y
+class A(object):
+
+	def __init__(self, x):
+		self.x = x
+
+	def combine(x, y):
+		if not x and y:
+			return y
+		if not y and x:
+			return x
+		if not x and not y:
+			return 'Both lists are empty'
+		else:
+			return x + y
